3b7ada1f29add807ac8a2747e57eac28b7c491bc
Sarah S <stms15@mun.ca>
2023-05-14 22:20:49 -0230

    added random character generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Validate the criteria data
 function validateCriteria(len, types) {
   var checkTypes = function(type) {
-    if (type === "false" || type === ""){
+    if (type === "false" || type === "" || type === "no"){
       return true;
     } else {
       return false;
@@ -24,8 +25,14 @@ function validateCriteria(len, types) {
   }
 }
 
+
 // Generate a random password based on the criteria
 function generatePassword() {
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var specialChars = "!@#$%^&*()-_+=?/{}";
+
   var passwordLen = prompt("Please enter the number of characters to use between 8 and 128");
   var includeLower = prompt("Do you want to inlcude lower case characters? (Ex: a, b...)\n" + "Enter: true/false");
   var includeUpper = prompt("Do you want to inlcude lower case characters? (Ex: A, B...)\n" + "Enter: true/false");
@@ -33,10 +40,32 @@ function generatePassword() {
   var includeSpecial = prompt("Do you want to inlcude lower case characters? (Ex: @, #...)\n" + "Enter: true/false");
 
   var characterTypes = [includeLower, includeUpper, includeNums, includeSpecial];
-
   var valid = validateCriteria(passwordLen, characterTypes);
-  console.log(valid);
+  
+  var selection = "";
+  if (characterTypes[0] == "true") {
+    selection = selection + lowerChars;
+  }
+  if (characterTypes[1] == "true") {
+    selection = selection + upperChars;
+  }
+  if (characterTypes[2] == "true") {
+    selection = selection + numbers;
+  }
+  if (characterTypes[3] == "true") {
+    selection = selection + specialChars;
+  }
+
+  var password = "";
+  if (valid) {
+    for (let j=0; j<passwordLen; j++) {
+      randomNum = Math.floor(Math.random() * selection.length);
+      password = password + selection[randomNum];
+    }
+    return password;
+  }
 }
+
 
 // Write password to the #password input
 function writePassword() {
